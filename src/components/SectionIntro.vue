64f9b60090cefb1cb294a6c94a1b423dab818eac
Casey Trombley <casey@algaecal.com>
2025-01-08 10:21:31 -0500

    anims tweak

--- src/components/SectionIntro.vue
+++ src/components/SectionIntro.vue
@@ -67,6 +67,7 @@ section {
 
   .text {
     position: absolute;
+    padding: 0 15px 0 2px;
     opacity: 0;
     transform: rotate(-1deg);
     animation: textAnim 0.3s ease-in-out forwards;
@@ -74,7 +75,7 @@ section {
   }
 
   .image {
-    //width: 70%;
+    width: 100%;
     //transform: translateX(190px);
     opacity: 0;
     animation: imageAnim 0.3s ease-in-out forwards;
@@ -104,12 +105,95 @@ section {
 
     .text {
       position: absolute;
-      //width: 65%
+      width: 95%
     }
 
     .image {
       width: 50%;
       transform: translateX(50%) translateY(0px);
+    }
+  }
+}
+
+@keyframes textAnim {
+  0% {
+    opacity: 0;
+    transform: translateX(-1000px) rotate(-1deg);
+  }
+  80% {
+    opacity: 1;
+    transform: translateX(20px) rotate(-1deg);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0) rotate(-1deg);
+  }
+}
+
+@keyframes textAnim2 {
+  0% {
+    transform: translateX(0) translateY(0) rotate(-1deg);
+  }
+  50% {
+    transform: translateX(0) translateY(5px) rotate(-1deg);
+  }
+  100% {
+    transform: translateX(0) translateY(0) rotate(-1deg);
+  }
+}
+
+@keyframes imageAnim {
+  0% {
+    opacity: 0;
+    transform: translateX(300%) translateY(-20px);
+  }
+  80% {
+    opacity: 1;
+    transform: translateX(55%) translateY(-20px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(50%) translateY(-20px);
+  }
+}
+
+@keyframes imageAnim2 {
+  0% {
+    transform: scale(1.3) skewX(6deg) skewY(-1deg);
+  }
+  50% {
+    transform: scale(1.4) skewX(3deg) skewY(-1deg);
+  }
+  100% {
+    transform: scale(1.3) skewX(6deg) skewY(-1deg);
+  }
+}
+
+@media (max-width: 599px) {
+  @keyframes imageAnim {
+    0% {
+      opacity: 0;
+      transform: translateX(300%) translateY(12px);
+    }
+    80% {
+      opacity: 1;
+      transform: translateX(25%) translateY(12px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateX(20%) translateY(12px);
+    }
+  }
+
+  @keyframes imageAnim2 {
+    0% {
+      transform: scale(1.5) skewX(6deg) skewY(-1deg);
+    }
+    50% {
+      transform: scale(1.6) skewX(3deg) skewY(-1deg);
+    }
+    100% {
+      transform: scale(1.5) skewX(6deg) skewY(-1deg);
     }
   }
 }
