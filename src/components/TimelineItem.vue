99573e8414431e8d4fd12266ba0edf197b666250
Casey Trombley <casey@algaecal.com>
2025-01-22 16:43:19 -0500

    tl

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,32 +1,42 @@
 <template>
-  <div class="text">
+  <div v-if="item === 0" class="text">
     <h4 class="timeline-title">
       The early days: 1990s - 2000s
     </h4>
     <p>
       Ah, the 1990s—a time when grunge was king, cassettes and floppy disks ruled supreme, and the internet was still wearing diapers. I spent my days in school, eventually making my way to college as the decade wrapped up, and my nights were filled with a deep love for graphic arts. Inspired by the vibrant worlds of Nintendo games and comic books, I dove headfirst into Photoshop and Illustrator, crafting questionable logos, chaotic gradients, and enough clip art to crash a printer.
-      <img src="/avatar.svg" alt="" class="float-right">
+      <img src="./avatar.svg" alt="" class="float-right">
     </p>
     <p>
       Then, one day, I stumbled upon the internet—this dazzling, pixelated frontier that felt like crossing an event horizon into the future. Suddenly, graphic design wasn’t enough. Enter Macromedia Studio: Dreamweaver, Flash, and Fireworks became my new tools of the trade. I built websites with spinning logos, animated buttons, and marquee text that screamed, “Welcome to 1999!” Flash was my muse—I was convinced it could make any website a masterpiece, as long as visitors didn’t mind waiting five minutes for it to load.
-      <img src="/avatar.svg" alt="" class="float-left">
+      <img src="./avatar.svg" alt="" class="float-left">
     </p>
   </div>
 
-  <div class="text">
+  <div v-if="item === 1" class="text">
     <h4 class="timeline-title">
       My Early 2000s to 2010s Adventure: A Web Developer’s Tale
     </h4>
     <p>
       Ah, the 1990s—a time when grunge was king, cassettes and floppy disks ruled supreme, and the internet was still wearing diapers. I spent my days in school, eventually making my way to college as the decade wrapped up, and my nights were filled with a deep love for graphic arts. Inspired by the vibrant worlds of Nintendo games and comic books, I dove headfirst into Photoshop and Illustrator, crafting questionable logos, chaotic gradients, and enough clip art to crash a printer.
-      <img src="/avatar.svg" alt="" class="float-right">
+      <img src="./avatar.svg" alt="" class="float-right">
     </p>
     <p>
       Then, one day, I stumbled upon the internet—this dazzling, pixelated frontier that felt like crossing an event horizon into the future. Suddenly, graphic design wasn’t enough. Enter Macromedia Studio: Dreamweaver, Flash, and Fireworks became my new tools of the trade. I built websites with spinning logos, animated buttons, and marquee text that screamed, “Welcome to 1999!” Flash was my muse—I was convinced it could make any website a masterpiece, as long as visitors didn’t mind waiting five minutes for it to load.
-      <img src="/avatar.svg" alt="" class="float-left">
+      <img src="./avatar.svg" alt="" class="float-left">
     </p>
   </div>
 </template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  item: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
 
 
 <style scoped lang="scss">
@@ -34,6 +44,11 @@ p {
   font-size: 1rem;
 }
 
+img {
+  width: 100px;
+  height: 100px;
+
+}
 .float-right {
   float: right;
   margin: 0 0 1rem 2rem;
