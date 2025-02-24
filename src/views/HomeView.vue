e652aaf0b6e149e0889f91173575f29b307b2d1f
Casey Trombley <casey@algaecal.com>
2025-02-24 12:31:08 -0500

    new proj

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,65 @@
 <template>
   <div class="home-view">
 
-<!--    <SectionArtwork />-->
-
     <SectionIntro />
     <SectionResume />
-    <SectionCaseStudies />
-    <SectionProjects />
-    <SectionDesigns />
-    <SectionTimeline />
-<!--    <SectionArtwork />-->
+
+    <!-- Lazy-load these sections -->
+    <Suspense>
+      <template #default>
+        <SectionCaseStudies />
+      </template>
+      <template #fallback>
+        <div>Loading Case Studies...</div>
+      </template>
+    </Suspense>
+
+    <Suspense>
+      <template #default>
+        <SectionProjects />
+      </template>
+      <template #fallback>
+        <div>Loading Projects...</div>
+      </template>
+    </Suspense>
+
+    <Suspense>
+      <template #default>
+        <SectionDesigns />
+      </template>
+      <template #fallback>
+        <div>Loading Designs...</div>
+      </template>
+    </Suspense>
+
+    <Suspense>
+      <template #default>
+        <SectionTimeline />
+      </template>
+      <template #fallback>
+        <div>Loading Timeline...</div>
+      </template>
+    </Suspense>
+
   </div>
 </template>
+
 <script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+
 import SectionIntro from "../components/SectionIntro.vue";
 import SectionResume from "../components/SectionResume.vue";
-import SectionTimeline from "../components/SectionTimeline.vue";
-//import SectionArtwork from "../components/SectionArtwork.vue";
-import SectionCaseStudies from "../components/SectionCaseStudies.vue";
-import SectionProjects from "../components/SectionProjects.vue";
-import SectionDesigns from "@/components/SectionDesigns.vue";
 
-
+// Lazy-load these sections
+const SectionCaseStudies = defineAsyncComponent(() => import('../components/SectionCaseStudies.vue'));
+const SectionProjects = defineAsyncComponent(() => import('../components/SectionProjects.vue'));
+const SectionDesigns = defineAsyncComponent(() => import('@/components/SectionDesigns.vue'));
+const SectionTimeline = defineAsyncComponent(() => import('../components/SectionTimeline.vue'));
 </script>
+
 <style lang="scss" scoped>
 .home-view {
   position: relative;
   overflow: visible;
 }
 </style>
-
