530756b2e6026e11bea452f9b675330b976f60e6
Casey Trombley <caseytrombley@gmail.com>
2025-05-01 14:56:07 -0400

    blog, contact

--- src/App.vue
+++ src/App.vue
@@ -19,14 +19,6 @@
     <AppFooter />
 
   </v-app>
-  <!-- Dummy Netlify form for static detection -->
-  <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-    <input type="hidden" name="form-name" value="contact" />
-    <input type="text" name="name" />
-    <input type="text" name="phone" />
-    <input type="email" name="email" />
-    <textarea name="message"></textarea>
-  </form>
 </template>
 
 <script setup lang="ts">
