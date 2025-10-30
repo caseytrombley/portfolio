e883726b1c9894fe1dc0e3adc8eb5811f8c5de5c
Boston Impressions <bostonimpressions@gmail.com>
2025-10-30 07:57:42 -0400

    update portfolio

--- src/components/SectionProjects.vue
+++ src/components/SectionProjects.vue
@@ -1,126 +1,138 @@
 <template>
   <section class="spaced">
     <v-container max-width="1200px" fluid class="container">
-<!--      <AppHeading-->
-<!--        color="light"-->
-<!--        title="Current Projects"-->
-<!--        subtitle="Let’s just say these projects are under construction... and always will be."-->
-<!--      />-->
+      <!--      <AppHeading-->
+      <!--        color="light"-->
+      <!--        title="Current Projects"-->
+      <!--        subtitle="Let’s just say these projects are under construction... and always will be."-->
+      <!--      />-->
 
       <TerminalWindow>
         <ul class="project-links" ref="listRef">
           <li>
-            <a
-              href="https://www.keyandchord.com"
-              target="_blank"
-              class="link"
-            >
-              <img src="/project-image-key-and-chord.jpg" alt="Key and Chord">
+            <a href="https://nexum-site.netlify.app/" target="_blank" class="link">
+              <img src="/project-image-nexum.jpg" alt="Nexum" />
               <div class="overlay">
                 <v-icon>mdi-open-in-new</v-icon>
               </div>
             </a>
             <div class="description">
-              An interactive piano chord dictionary with dynamic chord diagrams and inversions for all musical keys.
+              Leading both the Figma design and front-end development for Nexum’s new website,
+              creating refined, custom UI components that reflect the company’s authority in
+              cybersecurity and network defense.
             </div>
           </li>
 
           <li>
-            <a
-              href="https://jokesterapp.netlify.app/"
-              target="_blank"
-              class="link"
-            >
-              <img src="/project-image-jokester.jpg" alt="Jokester">
+            <a href="https://unlocked-home.netlify.app/" target="_blank" class="link">
+              <img src="/project-image-unlocked.jpg" alt="Unlocked Real Estate" />
               <div class="overlay">
                 <v-icon>mdi-open-in-new</v-icon>
               </div>
             </a>
             <div class="description">
-              Jokester is a fun, interactive app for browsing, rating, and adding jokes across categories like programming, general, and knock-knock. With features like custom joke creation, star ratings, and punchline reveals, it's built to bring laughs and a playful UI to your day — perfect for developers, friends, or anyone who needs a quick chuckle.
+              Developed a fully custom, responsive website for Unlocked Real Estate, translating
+              detailed Figma designs into polished, high-performance UI components.
             </div>
           </li>
 
           <li>
-            <a
-              href="https://wifilocator.netlify.app/"
-              target="_blank"
-              class="link"
-            >
-              <img src="/project-image-wifilocator.jpg" alt="WiFi Locator">
+            <a href="https://www.keyandchord.com" target="_blank" class="link">
+              <img src="/project-image-key-and-chord.jpg" alt="Key and Chord" />
               <div class="overlay">
                 <v-icon>mdi-open-in-new</v-icon>
               </div>
             </a>
             <div class="description">
-              WiFi Locator is an interactive map-based app that helps you find free public Wi-Fi hotspots in cities across the U.S. Just search for a city, and instantly explore nearby hotspots using real-time OpenStreetMap data — perfect for remote workers, travelers, and anyone in need of a reliable internet connection on the go.
+              An interactive piano chord dictionary with dynamic chord diagrams and inversions for
+              all musical keys.
             </div>
           </li>
 
           <li>
-            <a
-              href="https://crypto-keepr.netlify.app/"
-              target="_blank"
-              class="link"
-            >
-              <img src="/project-image-crypto.jpg" alt="Crypto Tracker">
+            <a href="https://jokesterapp.netlify.app/" target="_blank" class="link">
+              <img src="/project-image-jokester.jpg" alt="Jokester" />
               <div class="overlay">
                 <v-icon>mdi-open-in-new</v-icon>
               </div>
             </a>
             <div class="description">
-              Information site for cryptocurrencies. This is an ongoing side project in development. It leverages some open source API's for the data along with Chart.js for the visualizations.
+              Jokester is a fun, interactive app for browsing, rating, and adding jokes across
+              categories like programming, general, and knock-knock. With features like custom joke
+              creation, star ratings, and punchline reveals, it's built to bring laughs and a
+              playful UI to your day — perfect for developers, friends, or anyone who needs a quick
+              chuckle.
             </div>
           </li>
 
           <li>
-            <a
-              href="https://caseys-app-design-hadithi.netlify.app"
-              target="_blank"
-              class="link"
-            >
-              <img src="/project-image-hadithi.jpg" alt="Hadithi">
+            <a href="https://wifilocator.netlify.app/" target="_blank" class="link">
+              <img src="/project-image-wifilocator.jpg" alt="WiFi Locator" />
               <div class="overlay">
                 <v-icon>mdi-open-in-new</v-icon>
               </div>
             </a>
             <div class="description">
-              Website design for an Arts and Culture publication. This is the front end development site only. I have designed everything and built it with javascript, Vue, and CSS.
+              WiFi Locator is an interactive map-based app that helps you find free public Wi-Fi
+              hotspots in cities across the U.S. Just search for a city, and instantly explore
+              nearby hotspots using real-time OpenStreetMap data — perfect for remote workers,
+              travelers, and anyone in need of a reliable internet connection on the go.
             </div>
           </li>
 
+          <li>
+            <a href="https://crypto-keepr.netlify.app/" target="_blank" class="link">
+              <img src="/project-image-crypto.jpg" alt="Crypto Tracker" />
+              <div class="overlay">
+                <v-icon>mdi-open-in-new</v-icon>
+              </div>
+            </a>
+            <div class="description">
+              Information site for cryptocurrencies. This is an ongoing side project in development.
+              It leverages some open source API's for the data along with Chart.js for the
+              visualizations.
+            </div>
+          </li>
 
-
+          <li>
+            <a href="https://caseys-app-design-hadithi.netlify.app" target="_blank" class="link">
+              <img src="/project-image-hadithi.jpg" alt="Hadithi" />
+              <div class="overlay">
+                <v-icon>mdi-open-in-new</v-icon>
+              </div>
+            </a>
+            <div class="description">
+              Website design for an Arts and Culture publication. This is the front end development
+              site only. I have designed everything and built it with javascript, Vue, and CSS.
+            </div>
+          </li>
         </ul>
       </TerminalWindow>
 
-      <ProjectModal
-        v-model="isModalOpen"
-        :projectID="currentProjectID"
-      />
+      <ProjectModal v-model="isModalOpen" :projectID="currentProjectID" />
     </v-container>
   </section>
 </template>
 
 <script setup lang="ts">
-import { onMounted, onBeforeUnmount, ref, nextTick, watch } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-import ProjectModal from '../components/ProjectModal.vue';
-import TerminalWindow from "./TerminalWindow.vue";
-import AppHeading from "@/components/AppHeading.vue";
+import { onMounted, onBeforeUnmount, ref, nextTick, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import ProjectModal from '../components/ProjectModal.vue'
+import TerminalWindow from './TerminalWindow.vue'
+import AppHeading from '@/components/AppHeading.vue'
 
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
-const listRef = ref<HTMLElement | null>(null);
+const listRef = ref<HTMLElement | null>(null)
 
 onMounted(async () => {
-  await nextTick();
+  await nextTick()
 
-  const items = listRef.value?.querySelectorAll('li');
-  if (!items || items.length === 0) return;
+  const items = listRef.value?.querySelectorAll('li')
+  if (!items || items.length === 0) return
 
   items.forEach((item, i) => {
     gsap.fromTo(
@@ -136,57 +148,55 @@ onMounted(async () => {
           start: 'top 90%',
           toggleActions: 'play none none reverse',
           onEnter: () => {
-            const baseDelay = i * 150;
-            const randomOffset = Math.random() * 300;
+            const baseDelay = i * 150
+            const randomOffset = Math.random() * 300
             setTimeout(() => {
-              item.classList.add('glitch');
-            }, baseDelay + randomOffset);
+              item.classList.add('glitch')
+            }, baseDelay + randomOffset)
           },
           onLeaveBack: () => {
-            item.classList.remove('glitch');
+            item.classList.remove('glitch')
           },
         },
-      }
-    );
-  });
+      },
+    )
+  })
 
-  requestAnimationFrame(() => ScrollTrigger.refresh());
-});
+  requestAnimationFrame(() => ScrollTrigger.refresh())
+})
 
 onBeforeUnmount(() => {
-  ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-});
+  ScrollTrigger.getAll().forEach((trigger) => trigger.kill())
+})
 
-const currentProjectID = ref<string | null>(null);
-const isModalOpen = ref(false);
+const currentProjectID = ref<string | null>(null)
+const isModalOpen = ref(false)
 
-const route = useRoute();
-const router = useRouter();
+const route = useRoute()
+const router = useRouter()
 
 watch(
   () => route.params.projectID,
   (projectID) => {
     if (projectID) {
-      currentProjectID.value = projectID as string;
-      isModalOpen.value = true;
+      currentProjectID.value = projectID as string
+      isModalOpen.value = true
     } else {
-      currentProjectID.value = null;
-      isModalOpen.value = false;
+      currentProjectID.value = null
+      isModalOpen.value = false
     }
   },
-  { immediate: true }
-);
+  { immediate: true },
+)
 
 watch(isModalOpen, (isOpen) => {
   if (!isOpen) {
-    router.push({ name: 'home' });
+    router.push({ name: 'home' })
   }
-});
+})
 </script>
 
-
 <style lang="scss" scoped>
-
 $blue: #25e0cb;
 
 section {
@@ -248,7 +258,9 @@ section {
     width: 100%;
     height: 100%;
     //background: #25e0cb;
-    box-shadow: rgba(37, 224, 203, 0.12) 0 1px 3px, rgba(37, 224, 203, 0.24) 0px 1px 2px;
+    box-shadow:
+      rgba(37, 224, 203, 0.12) 0 1px 3px,
+      rgba(37, 224, 203, 0.24) 0px 1px 2px;
     border-top: 3px solid #0e6e66;
     border-right: 2px solid #0c4b44;
     border-left: 4px solid #0e6e66;
@@ -258,9 +270,8 @@ section {
     z-index: 2;
     //opacity: 0;
     transform: translate(0, 0);
-    opacity: .5;
+    opacity: 0.5;
   }
-
 
   img {
     width: 100%;
@@ -313,11 +324,9 @@ section {
 }
 
 @media (min-width: 600px) {
-
   .link {
     min-width: 160px;
     height: 160px;
-
   }
 
   .description {
@@ -325,5 +334,4 @@ section {
     line-height: 1;
   }
 }
-
 </style>
